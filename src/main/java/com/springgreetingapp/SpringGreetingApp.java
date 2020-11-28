6d93549147da03372fcaeb2ab01cf2c9caccde73
unknown <divyacharaninabha@gmail.com>
2020-11-28 22:23:29 +0530

    Added: Spring application added

---
+++ src/main/java/com/springgreetingapp/SpringGreetingApp.java
@@ -0,0 +1,13 @@
+package com.springgreetingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGreetingApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGreetingApp.class, args);
+	}
+
+}
